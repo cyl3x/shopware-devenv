d97d8c0571106df29abf28d3cffe48ae9010fbea
cyl3x <cyl3x@posteo.de>
2023-12-07 21:22:52 +0100

    Release 0.8.0 Dependcy update

--- src/operations/watch.rs
+++ src/operations/watch.rs
@@ -3,7 +3,7 @@ use crate::{direnv, Command};
 pub fn admin() -> anyhow::Result<String> {
     direnv!["composer", "watch:admin"]
         .await_success()
-        .map(|_| "Watcher stopped".into())
+        .map(|()| "Watcher stopped".into())
 }
 
 pub fn storefront() -> anyhow::Result<String> {
@@ -13,17 +13,17 @@ pub fn storefront() -> anyhow::Result<String> {
 
     direnv!["composer", "watch:storefront"]
         .await_success()
-        .map(|_| "Watcher stopped".into())
+        .map(|()| "Watcher stopped".into())
 }
 
 pub fn admin_jest() -> anyhow::Result<String> {
     direnv!["composer", "admin:unit:watch"]
         .await_success()
-        .map(|_| "Watcher stopped".into())
+        .map(|()| "Watcher stopped".into())
 }
 
 pub fn storefront_jest() -> anyhow::Result<String> {
     direnv!["composer", "storefront:unit:watch"]
         .await_success()
-        .map(|_| "Watcher stopped".into())
+        .map(|()| "Watcher stopped".into())
 }
