844155e692bab40979f3f9d1d464102073aa1048
cyl3x <cyl3x@posteo.de>
2024-01-22 20:55:08 +0100

    Removed option to stop devenv by killing nix store processes

--- src/operations/down.rs
+++ src/operations/down.rs
@@ -1,6 +1,6 @@
 use sysinfo::{Pid, ProcessExt, Signal, System, SystemExt};
 
-use crate::{topic, y_or_n, Context};
+use crate::{topic, Context};
 
 pub fn main() -> anyhow::Result<String> {
     topic!("Stopping...");
@@ -10,7 +10,7 @@ pub fn main() -> anyhow::Result<String> {
 
     Context::get()?
         .devenv_pid()
-        .map_or_else(|_| down_by_process(&sys), |pid| down_by_pid(&sys, pid))?;
+        .map_or_else(|_| anyhow::bail!("Unable to find devenv's process"), |pid| down_by_pid(&sys, pid))?;
 
     Ok("Devenv service stopped".into())
 }
@@ -26,32 +26,4 @@ fn down_by_pid(sys: &System, pid: usize) -> anyhow::Result<()> {
     }
 
     anyhow::bail!("Devenv was not running");
-}
-
-fn down_by_process(sys: &System) -> anyhow::Result<()> {
-    topic!("Missing pidfile, choose...");
-    println!("Pidfile was not found, but you can kill all Nix processes to stop devenv");
-
-    if !y_or_n!("Kill all Nix processes?") {
-        anyhow::bail!("Aborted killing all Nix processes");
-    }
-
-    sys.processes()
-        .values()
-        .filter(|process| {
-            if !process.exe().starts_with("/nix/store") {
-                return false;
-            }
-
-            println!(
-                "Killing {} {}...",
-                process.pid(),
-                process.exe().to_string_lossy()
-            );
-
-            process.kill_with(Signal::Interrupt).unwrap_or_default()
-        })
-        .for_each(sysinfo::ProcessExt::wait);
-
-    Ok(())
 }
